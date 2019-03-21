0c7ddbb5bcd97fec7b93a8d219b7eb5b8262f5a1
s33dco <code@s33d.co>
2019-03-21 11:20:15 +0000

    init commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log('app.js is running');
+
+// JSX - Javascript XML
+
+let template = <h1>Indecision App</h1>;
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template,appRoot);
