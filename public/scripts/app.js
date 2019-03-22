602ae492ea985681f458545f0ab8fdfad9a162d9
s33dco <code@s33d.co>
2019-03-22 00:11:30 +0000

    binding rerendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,145 @@ console.log('app.js is running');
 
 // JSX - Javascript XML
 
+var app = {
+    title: 'Indecision App.',
+    subTitle: 'Make up your mind.',
+    options: ['Option One', 'Option Two']
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subTitle && React.createElement(
+        'p',
+        null,
+        app.subTitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: "Sir Loveton Supreme",
+    age: 48,
+    location: 'Brighton'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location : ',
+            location
+        );
+    }
+}
+
+var count = 0;
+
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count : ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count : ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'reset'
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
+
+// const multiplier = {
+//     numbers : [12, 24,26,57,78,345],
+//     multiplyBy: 13,
+//     multiply(){
+//         return this.numbers.map((number)=> number * this.multiplyBy);
+//     }
+// }
+
+// console.log(multiplier.multiply());
--- src/app.js
+++ src/app.js
@@ -2,7 +2,89 @@ console.log('app.js is running');
 
 // JSX - Javascript XML
 
-let template = <h1>Indecision App</h1>;
-let appRoot = document.getElementById('app');
+const app = {
+    title: 'Indecision App.',
+    subTitle: 'Make up your mind.',
+    options: ['Option One', 'Option Two']
+}
 
-ReactDOM.render(template,appRoot);
+const template =(
+    <div>
+        <h1>{app.title}</h1>
+        { app.subTitle && <p>{app.subTitle}</p>}
+        <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const user = {
+    name : "Sir Loveton Supreme",
+    age : 48,
+    location: 'Brighton'
+}
+
+function getLocation(location) {
+    if (location){
+        return <p>Location : {location}</p>;
+    }
+}
+
+
+let count = 0
+
+const addOne = () => {
+    count++;
+    renderCounterApp();
+} 
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+} 
+
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+} 
+
+const templateTwo = (
+    <div>
+        <h1>Count : {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>reset</button>
+    </div>
+);
+
+const appRoot = document.getElementById('app');
+
+
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count : {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo,appRoot);
+    
+}
+
+
+renderCounterApp();
+
+// const multiplier = {
+//     numbers : [12, 24,26,57,78,345],
+//     multiplyBy: 13,
+//     multiply(){
+//         return this.numbers.map((number)=> number * this.multiplyBy);
+//     }
+// }
+
+// console.log(multiplier.multiply());
