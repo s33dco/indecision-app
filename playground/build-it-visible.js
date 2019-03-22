8121d2631f5345c61e69450e6dab23a8834e6228
s33dco <code@s33d.co>
2019-03-22 13:18:39 +0000

    visibility toggle

---
+++ playground/build-it-visible.js
@@ -0,0 +1,29 @@
+console.log('app.js is running');
+
+let visibility = false ;
+
+const toggleVisibility = () => {
+    visibility = !visibility;
+    render();
+};
+
+const render = () => {
+    const jsx = (
+        <div>
+            <h1>Visibility Toggle</h1>
+
+            <button onClick={toggleVisibility}>
+            {visibility ? 'Hide Details' : 'Show details'}
+            </button>
+            {visibility && (
+                <div>
+                    <p>Now you see me</p>
+                </div>
+
+            )}
+        </div>
+    )
+    ReactDOM.render(jsx, document.getElementById('app'));
+}
+
+render();
