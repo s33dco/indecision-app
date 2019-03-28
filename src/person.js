7ee0a7068eb72908fbf43799c371610185670e4d
s33dco <code@s33d.co>
2019-03-28 14:01:49 +0000

    working app before splitting into components

--- src/person.js
+++
@@ -1,9 +0,0 @@
-console.log('person.js is running');
-
-const isAdult = (age) => age >= 18;
-
-const canDrink = (age) => age >= 18;
-
-export default (age) => age >= 65;
-
-export {isAdult, canDrink}
